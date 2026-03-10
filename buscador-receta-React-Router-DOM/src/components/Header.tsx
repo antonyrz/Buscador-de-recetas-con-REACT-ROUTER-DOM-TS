05534a4ceab09b25a909c9bcf162d93022c481aa
Antonyhz <hernandezantony333@gmail.com>
2026-03-10 16:02:57 -0400

    Creando formulario de busqueda

--- buscador-receta-React-Router-DOM/src/components/Header.tsx
+++ buscador-receta-React-Router-DOM/src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { useMemo } from "react";
 import { useLocation, NavLink } from "react-router-dom"
 
 export default function Header() {
 
     const {pathname} = useLocation();
 
-    console.log(pathname)
+    const isHome = useMemo(() => pathname === '/', [pathname]);
+
+    console.log(isHome)
 
   return (
         <header className='bg-slate-800'>
@@ -31,6 +34,44 @@ export default function Header() {
                         </NavLink>
                     </nav>
                 </div>
+
+                {isHome && (
+                    <form className="md:1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                        <div className="space-y-4">
+                            <label htmlFor="busqueda"
+                            className="block text-white uppercase font-extrabold text-lg"
+                            >Nombres o Ingredientes
+                            </label>
+
+                            <input 
+                                type="text"
+                                id="busqueda"
+                                name="busqueda"
+                                className="w-full p-3 bg-white rounded-lg focus:outline-none"
+                                placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Café"
+                            />
+                        </div>
+
+                        <div className="space-y-4">
+                            <label htmlFor="category"
+                            className="block text-white uppercase font-extrabold text-lg"
+                            >Categoría
+                            </label>
+
+                            <select
+                                id="category"
+                                name="category"
+                                className="w-full p-3 bg-white rounded-lg focus:outline-none"
+                            >
+                                <option value="" selected disabled>-- Seleccione --</option>
+                            </select>
+
+                        </div>
+
+                        <input type="submit" value="Buscar Recetas" 
+                        className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"/>
+                    </form>
+                )}
             </div>
         </header>
     )
